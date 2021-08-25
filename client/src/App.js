b4d008111445d75eeab77378424f3ee088567fbb
Anthony-Laptop <anthonykrueger0@gmail.com>
2021-08-25 07:49:56 -0500

    minor tweaks

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import React from "react";
 
 import Projects from "./pages/Projects"
 import Home from "./pages/Home"
+import AboutMe from "./pages/AboutMe"
 import CustomNavbar from "./components/Navbar"
 
 
@@ -13,8 +14,9 @@ function App() {
   return (
     <div id="main" className="h-screen flex bg-theme-main overflow-hidden">
       <CustomNavbar />
-      <div id="content" className="flex flex-col py-5 space-y-20 w-full overflow-y-auto">
+      <div id="content" className="flex flex-col py-5 mx-7 space-y-20 w-full overflow-y-auto">
           <Home />
+          <AboutMe />
           <Projects />
       </div>
     </div>
---
+++ client/src/pages/AboutMe.js
@@ -0,0 +1,11 @@
+import Header from "../components/Header"
+
+export default function AboutMe() {
+    return (
+        <div className="min-h-screen my-5 container mx-auto rounded shadow-sm bg-theme-light">
+            <div id="aboutme">
+                <Header text="About Me" />
+            </div>
+        </div>
+    )
+}
